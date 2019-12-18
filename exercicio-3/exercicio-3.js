87ced44dfb116e65080d4e5d5b3758bb8979b484
Carlos <fer.1972@hotmail.com>
2019-12-18 17:00:40 -0300

    exercicio 3

---
+++ exercicio-3/exercicio-3.js
@@ -0,0 +1,5 @@
+var skills = ["Javascript", "ReactJS", "React Native"];
+
+function temHabilidade(skills) {
+    return skills.indexOf("Javascript") == 0 ? console.log(true) : console.log(false);
+};
