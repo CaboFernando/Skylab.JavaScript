7a74c68aadc1473afb1c1f1ac03f2d34dec77792
Carlos <fer.1972@hotmail.com>
2019-12-19 11:44:05 -0300

    trabalhando com a DOM

--- modulo-2/index.js
+++ modulo-2/index.js
@@ -1,3 +1,20 @@
-mostraAlerta = () => {
-    alert('Botão foi clicador');
+// mostraAlerta = () => {
+//     alert('Botão foi clicador');
+// };
+
+
+// var inputElement;
+// inputElement = document.getElementById('nome');
+// inputElement = document.getElementsByTagName('input')[0];
+// inputElement = document.getElementsByClassName('nome')[0];
+// inputElement = document.querySelector('div#app input');
+// inputElement = document.querySelector('input[name=nome]');
+// inputElement = document.querySelectorAll('input');
+// console.log(inputElement);
+
+var inputElement = document.querySelector('input[name=nome]');
+var btnElement = document.querySelector('button.botao');
+btnElement.onClick = mostra = () => {
+    alert(inputElement.value);
 };
+
