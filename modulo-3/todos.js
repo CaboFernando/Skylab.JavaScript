fbafba9662354bcd3d5eca049dc8739fb10d55d9
Carlos <fer.1972@hotmail.com>
2019-12-23 11:39:40 -0300

    iniciando aplicação

---
+++ modulo-3/todos.js
@@ -0,0 +1,12 @@
+
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Comer',
+    'Codas',
+    'Dormir',
+    'Repetir'
+];
+
