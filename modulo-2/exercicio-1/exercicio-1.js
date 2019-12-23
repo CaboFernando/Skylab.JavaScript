e2b271cc072218007dd757a5556e2d7f2c7118e1
Carlos <fer.1972@hotmail.com>
2019-12-23 08:57:59 -0300

    melhoria do exercicio 1

--- modulo-2/exercicio-1/exercicio-1.js
+++ modulo-2/exercicio-1/exercicio-1.js
@@ -1,13 +1,15 @@
+var btnElement = document.querySelector('button.btn');
+let count = 1;
 
-var btnElement = document.querySelector('#btn');
-
-mostra = () => {
-    let boxElement = document.createElement("div");
-    boxElement.style.width = '100px';
-    boxElement.style.height = '100px';
-    boxElement.style.backgroundColor = '#f00';
-
-    boxElement.classList.add('box');
-
-    document.body.appendChild(boxElement);
-};
+btnElement.onclick = function() {
+  let boxElement = document.createElement("div");
+  boxElement.className = "box";
+  boxElement.id = "box-" + count;
+  boxElement.style.width = '100px';
+  boxElement.style.height = '100px';
+  boxElement.style.marginTop = '10px';
+  boxElement.style.backgroundColor = '#f00';
+  
+  document.getElementById('app').appendChild(boxElement);
+  count++;
+}
