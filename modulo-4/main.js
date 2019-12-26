f177a0ddb124dcc9538df3b046d665b87925ff02
Carlos <fer.1972@hotmail.com>
2019-12-26 15:38:37 -0300

    utilizando axios

--- modulo-4/main.js
+++ modulo-4/main.js
@@ -11,24 +11,25 @@
 // }
 
 // Com Promises
-var myPromise = () => {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/CaboFernando');
-        xhr.send(null);
+// var myPromise = () => {
+//     return new Promise(function (resolve, reject) {
+//         var xhr = new XMLHttpRequest();
+//         xhr.open('GET', 'https://api.github.com/users/CaboFernando');
+//         xhr.send(null);
 
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200)
-                    resolve(JSON.parse(xhr.responseText));
-                else
-                    reject('Erro na Requisição');
-            }
-        }
-    });
-};
+//         xhr.onreadystatechange = () => {
+//             if (xhr.readyState === 4) {
+//                 if (xhr.status === 200)
+//                     resolve(JSON.parse(xhr.responseText));
+//                 else
+//                     reject('Erro na Requisição');
+//             }
+//         }
+//     });
+// };
 
-myPromise()
+// Com Axios
+axios.get('https://api.github.com/users/CaboFernando')
     .then(function (response) {
         console.log(response);
     })
