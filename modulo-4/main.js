e53e0b50cbb97e3fee13ea36e538400059dbdb73
Carlos <fer.1972@hotmail.com>
2019-12-26 15:23:06 -0300

    promises

--- modulo-4/main.js
+++ modulo-4/main.js
@@ -1,10 +1,37 @@
-var xhr = new XMLHttpRequest();
+// Sem Promises
+// var xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://api.github.com/users/CaboFernando');
-xhr.send(null);
+// xhr.open('GET', 'https://api.github.com/users/CaboFernando');
+// xhr.send(null);
 
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText));
-    }
-}
+// xhr.onreadystatechange = () => {
+//     if (xhr.readyState === 4){
+//         console.log(JSON.parse(xhr.responseText));
+//     }
+// }
+
+// Com Promises
+var myPromise = () => {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/CaboFernando');
+        xhr.send(null);
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200)
+                    resolve(JSON.parse(xhr.responseText));
+                else
+                    reject('Erro na Requisição');
+            }
+        }
+    });
+};
+
+myPromise()
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.warn(error);
+    });
